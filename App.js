80c41c8e05b800721e0fe2266822d7f028481042
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-12-18 19:55:59 +0100

    talha 2. deneme

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ function App() {
       <Text>deneme 1</Text>
       <Text>This is the first row created by eyup</Text>
       <Text>This is the second row created by eyup</Text>
-      <Text>Teamwork - Talha deneme 1</Text>
+      <p>Teamwork - Talha deneme 1</p>
       <h1>Pull request 3</h1>
     </div>
   );
